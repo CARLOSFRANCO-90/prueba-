4f1fb08dc49a147498e067d504e754621e5375cd
CARLOSFRANCO-90 <carlos.franco9015@gmail.com>
2022-09-02 14:08:18 -0500

    cambio A

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+const port = 5000
+
+app.listen(port, ()=>{console.log('connet with the port', port)})
